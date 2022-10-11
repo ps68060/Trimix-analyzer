caa36705e3094176a75e152033df09f73600bf9c
peters <pslegg@scubadivers.co.uk>
2022-10-11 21:39:21 +0100

    English version.

--- src/trimix-analyzer.ino
+++ src/trimix-analyzer.ino
@@ -3,39 +3,39 @@
  * Arduino project "trimix-analyzer.ino" de Yves Cazé E3 ASPTT Lyon
 \*********************************************************************************************/
 #include <Wire.h>
-#include <Adafruit_ADS1015.h>
+#include <Adafruit_ADS1X15.h>
 #include <RunningAverage.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);  // set the LCD address to 0x27 for a 20 chars and 4 line display
-Adafruit_ADS1115 ads;    // convertisseur analogique --> digital ADS1115
+LiquidCrystal_I2C lcd(0x3F, 20, 4);  // set the LCD address to 0x3F for a 20 chars and 4 line display
+Adafruit_ADS1115 ads;        // convertisseur analogique --> digital ADS1115
 
 // variables will change:
-float TensionCalib = 0;      // mise a 0 de la tension de calibrage de la cellule
-float voltage = 0;           // tension mesuree sur cellule
-float wheatstone = 0;        // tension sur pont de wheatsone
+float TensionCalib = 0;      // mise a 0 de la tension de calibrage de la cellule / setting to 0 of the cell calibration voltage
+float voltage = 0;           // tension mesuree sur cellule / voltage measured on cell
+float wheatstone = 0;        // tension sur pont de wheatsone / wheatsone bridge voltage
 
-float gain = 0.03125;      // gain du convertisseur ADS1115
-float calibMD62 = 661.26;      // valeur de la tension du pont avec 100% helium
-float WheatCalib = 10;      // mise a 0 de la tension de calibrage du pont à l'air
-float CorrFroid = 0 ;        // correction de lecture du fait de lecture a froid (capteur pas assez chaud)
-unsigned long time;          // mesure du temps depuis allumage pour ajuster correction
+float gain = 0.03125;        // gain du convertisseur ADS1115 / ADS1115 converter gain
+float calibMD62 = 661.26;    // valeur de la tension du pont avec 100% helium / bridge voltage value with 100% helium
+float WheatCalib = 10;       // mise a 0 de la tension de calibrage du pont à l'air / setting to 0 of the calibration voltage of the air bridge
+float CorrFroid = 0 ;        // correction de lecture du fait de lecture a froid (capteur pas assez chaud) / reading correction due to cold reading (sensor not hot enough)
+unsigned long time;          // mesure du temps depuis allumage pour ajuster correction / measurement of time since ignition to adjust correction
 
-RunningAverage RA0(10);       // moyennage O2 sur 10 valeurs
-RunningAverage RA1(10);       // moyennage He sur 10 valeurs
+RunningAverage RA0(10);      // moyennage O2 sur 10 valeurs / O2 averaging on 10 values
+RunningAverage RA1(10);      // moyennage He sur 10 valeurs / He averaging on 10 values
 
 
 /*********************************************************************************************\
  * SETUP
 \*********************************************************************************************/
 void setup()
-{
+{  
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);  
   
-  lcd.begin();
+  lcd.init();
   lcd.backlight();
-  lcd.print("  Analyseur Trimix");
+  lcd.print("  Trimix Analyser");
   
   ads.setGain(GAIN_FOUR); // 4x gain 1 bit = 0.03125mV
   ads.begin();
@@ -51,24 +51,26 @@ void setup()
   RA1.addValue(adc1);
   wheatstone = RA1.getAverage()*gain;
  
-  // affichage de la tension 
+  // voltage display 
   lcd.setCursor(0,2);
   lcd.print("V cell = ");
   lcd.print(voltage,2);
   lcd.print("mV");
   lcd.setCursor(0,3);
-  lcd.print("V pont = ");
+  lcd.print("V bridge = ");
   lcd.print(wheatstone,2);
   lcd.print("mV");
   delay(2000);
-  
+
+  // Begin calibration
   lcd.clear();
   lcd.setCursor(0,1);
-  lcd.print(" Calib. en cours ...");
+  lcd.print(" Calib. in progress");
   lcd.setCursor(0,2);
-  lcd.print("(utiliser de l'air)");
+  lcd.print("(using air)");
   
-  // determination de la tension moyenne de la cellule à l'air libre
+  // determination de la tension moyenne de la cellule à l'air libre /
+  // determination of the average voltage of the cell in the open air 
   int i = 0;
   float tensionMoyenne = 0;
   
@@ -76,27 +78,29 @@ void setup()
   {
     adc0 = ads.readADC_Differential_0_1();
     RA0.addValue(adc0);
-    voltage = abs(RA0.getAverage()*gain);
+    voltage = abs(RA0.getAverage()*gain); 
     tensionMoyenne = tensionMoyenne + voltage;
     delay(200);
   }
-     
+
   lcd.clear();
   lcd.setCursor(0,1);
-  lcd.print("    Calibrage OK");
+  lcd.print("  Calibration OK");
   tensionMoyenne = tensionMoyenne / (i - 1);
   TensionCalib = tensionMoyenne;
   lcd.setCursor(0,2);
   lcd.print("V calib = ");
   lcd.print(TensionCalib,2);
   lcd.print("mV");
-      
+  // End calibration
+   
   delay(2000);
+
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Prechauffage du");
+  lcd.print("Preheating of the");
   lcd.setCursor(0,1);
-  lcd.print("  capteur Helium...");
+  lcd.print("  Helium sensor...");
   delay(500);
    
   while(wheatstone > 10)
@@ -104,20 +108,21 @@ void setup()
     adc1 = ads.readADC_Differential_2_3();
     RA1.addValue(adc1);
     wheatstone = RA1.getAverage()*gain;
-    lcd.setCursor(4,3);
-    lcd.print("Vpont=");
+    lcd.setCursor(4,3);    
+    lcd.print("Vbridge=");
     lcd.print(wheatstone,0);
-    lcd.print("mV  ");
+    lcd.print("mV  "); 
     delay(50);
-
+  }
          
   lcd.clear();
   lcd.setCursor(0,1);
-  lcd.print(" Capteur He OK");
+  lcd.print(" He sensor OK");           
   delay(2000);
   lcd.clear();
 
 }
+
 
 void loop()
 {
@@ -128,7 +133,7 @@ void loop()
   adc0 = ads.readADC_Differential_0_1();
   adc1 = ads.readADC_Differential_2_3();
   
-  time = millis();      // temps en ms depuis démarrage programme
+  time = millis();      // temps en ms depuis démarrage programme / time in ms since program startup
 
   RA0.addValue(adc0);
   voltage = abs(RA0.getAverage()*gain);
@@ -137,41 +142,43 @@ void loop()
   wheatstone = RA1.getAverage()*gain;
 
   float nitrox = 0;
-  int MOD = 0;                 // valeur de MOD du mélange
-  int EAD = 0;                  // prof equivalente Air pour narcose 30m
+  int MOD = 0;                  // valeur de MOD du mélange / MOD value of the mixture
+  int EAD = 0;                  // prof equivalente Air pour narcose 30m / prof equivalent Air for narcosis 30m
   float helium = 0;
   
   nitrox = voltage * (20.9 / TensionCalib);
   MOD = 10 * ( (160/nitrox) - 1);
     
   lcd.setCursor(0,0);
-  if (voltage > 1) {
-    lcd.print("Oxygene = ");
+  if (voltage > 1)
+  {
+    lcd.print("Oxygen = ");
     lcd.print(nitrox,1);
     lcd.print("%   ");
     lcd.setCursor(12,2);
     lcd.print(" MOD");
     lcd.print(MOD);
     lcd.print("m");
+
     if (MOD < 100)
     {
-      lcd.print(" ");
-     }
+      lcd.print(" "); 
+    }
   }
   else
   {
-    lcd.print("Cell O2 HS !!! ");
+    lcd.print("!  Cell O2 Error   !");
   }
 
-  lcd.setCursor(0,3);
-  lcd.print("Vpont=");
+  lcd.setCursor(0,3);    
+  lcd.print("Vbridge=");
   lcd.print(wheatstone,0);
-  lcd.print("mV  ");
+  lcd.print("mV  "); 
 
-  wheatstone = wheatstone - WheatCalib;
+  wheatstone = wheatstone - WheatCalib;  
     
-  if (time < 480000) {  CorrFroid = 1 ; }     // correction de la tension lue en fonction du temps de chauffe du capteur
-  if (time < 360000) {  CorrFroid = 2 ; }
+  if (time < 480000) {  CorrFroid = 1 ; }     // correction de la tension lue en fonction du temps de chauffe du capteur 
+  if (time < 360000) {  CorrFroid = 2 ; }     // correction of the read voltage according to the heating time of the sensor
   if (time < 300000) {  CorrFroid = 3 ; }
   if (time < 270000) {  CorrFroid = 4 ; }
   if (time < 240000) {  CorrFroid = 5 ; }
@@ -181,35 +188,37 @@ void loop()
   if (time < 150000) {  CorrFroid = 9 ; }
   if (time < 120000) {  CorrFroid = 10 ; }
   if (time < 105000) {  CorrFroid = 11 ; }
-  if (time < 90000) {  CorrFroid = 12 ; }
-  if (time < 80000) {  CorrFroid = 13 ; }
-  if (time < 70000) {  CorrFroid = 14 ; }
-  if (time < 60000) {  CorrFroid = 15 ; }
-  if (time < 50000) {  CorrFroid = 16 ; }
-  if (time < 40000) {  CorrFroid = 17 ; }
-  if (time < 30000) {  CorrFroid = 18 ; }
-
-  wheatstone = wheatstone - CorrFroid;            // ajustement car capteur pas assez chaud
+  if (time < 90000)  {  CorrFroid = 12 ; }
+  if (time < 80000)  {  CorrFroid = 13 ; }
+  if (time < 70000)  {  CorrFroid = 14 ; }
+  if (time < 60000)  {  CorrFroid = 15 ; }
+  if (time < 50000)  {  CorrFroid = 16 ; }
+  if (time < 40000)  {  CorrFroid = 17 ; }
+  if (time < 30000)  {  CorrFroid = 18 ; }
+  
+  wheatstone = wheatstone - CorrFroid;            // ajustement car capteur pas assez chaud / adjustment because sensor not hot enough
    
-  lcd.setCursor(0,1);
+  lcd.setCursor(0,1);    
   lcd.print("Helium = ");
   helium = 100 * wheatstone / calibMD62;
+
   if (helium > 50)
   {
     helium = helium * (1 + (helium - 50) * 0.4 / 100);
   }
+
   if (helium > 2)
   {
     lcd.print(helium,1);
-    lcd.print("%    ");
+    lcd.print("%    "); 
   }
   else
   {
     helium = 0;
-    lcd.print("0%    ");
+    lcd.print("0%    ");  
   }
 
-  lcd.setCursor(0,2);
+  lcd.setCursor(0,2);  
   if (helium > 0)
   {
     lcd.print("Trimix ");
@@ -221,11 +230,11 @@ void loop()
     lcd.print(" EAD");
     EAD = 10 * ( 100 * 3.2 / (100 - helium - nitrox) -1 );
     lcd.print(EAD);
-    lcd.print("m");
+    lcd.print("m");  
     if (EAD < 100)
     {
-      lcd.print(" ");
-    }
+      lcd.print(" "); 
+    } 
   }
   else
   {
@@ -234,7 +243,5 @@ void loop()
     lcd.print("    ");
     lcd.setCursor(12,3);
     lcd.print("       ");
-  }
-
+  }  
 }
-
