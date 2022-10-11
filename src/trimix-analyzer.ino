08892052bd9d2e990f71409ef203603cdfdae5bc
peters <pslegg@scubadivers.co.uk>
2022-10-11 21:29:30 +0100

    Indents and braces.

--- src/trimix-analyzer.ino
+++ src/trimix-analyzer.ino
@@ -28,8 +28,8 @@ RunningAverage RA1(10);       // moyennage He sur 10 valeurs
 /*********************************************************************************************\
  * SETUP
 \*********************************************************************************************/
-void setup() {
-  
+void setup()
+{
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);  
   
@@ -62,63 +62,65 @@ void setup() {
   lcd.print("mV");
   delay(2000);
   
-   lcd.clear();
-   lcd.setCursor(0,1);
-   lcd.print(" Calib. en cours ...");
-   lcd.setCursor(0,2);
-   lcd.print("(utiliser de l'air)");
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print(" Calib. en cours ...");
+  lcd.setCursor(0,2);
+  lcd.print("(utiliser de l'air)");
   
   // determination de la tension moyenne de la cellule à l'air libre
   int i = 0;
   float tensionMoyenne = 0;
   
   for(i = 1; i <10 or (abs (voltage - (tensionMoyenne / (i-1)))) > 0.001; i++) 
-    {
-      adc0 = ads.readADC_Differential_0_1();
-      RA0.addValue(adc0);
-      voltage = abs(RA0.getAverage()*gain); 
-      tensionMoyenne = tensionMoyenne + voltage;
-      delay(200);
-    }
+  {
+    adc0 = ads.readADC_Differential_0_1();
+    RA0.addValue(adc0);
+    voltage = abs(RA0.getAverage()*gain);
+    tensionMoyenne = tensionMoyenne + voltage;
+    delay(200);
+  }
      
-   lcd.clear();
-   lcd.setCursor(0,1);
-   lcd.print("    Calibrage OK");
-   tensionMoyenne = tensionMoyenne / (i - 1);
-   TensionCalib = tensionMoyenne;
-   lcd.setCursor(0,2);
-   lcd.print("V calib = ");
-   lcd.print(TensionCalib,2);
-   lcd.print("mV");
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("    Calibrage OK");
+  tensionMoyenne = tensionMoyenne / (i - 1);
+  TensionCalib = tensionMoyenne;
+  lcd.setCursor(0,2);
+  lcd.print("V calib = ");
+  lcd.print(TensionCalib,2);
+  lcd.print("mV");
       
-   delay(2000);
-   lcd.clear();
-   lcd.setCursor(0,0);
-   lcd.print("Prechauffage du");
-   lcd.setCursor(0,1);
-   lcd.print("  capteur Helium...");
-   delay(500);
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Prechauffage du");
+  lcd.setCursor(0,1);
+  lcd.print("  capteur Helium...");
+  delay(500);
    
-   while(wheatstone > 10){
-         adc1 = ads.readADC_Differential_2_3();
-         RA1.addValue(adc1);
-         wheatstone = RA1.getAverage()*gain;
-         lcd.setCursor(4,3);    
-         lcd.print("Vpont=");
-         lcd.print(wheatstone,0);
-         lcd.print("mV  "); 
-         delay(50);
-         }
+  while(wheatstone > 10)
+  {
+    adc1 = ads.readADC_Differential_2_3();
+    RA1.addValue(adc1);
+    wheatstone = RA1.getAverage()*gain;
+    lcd.setCursor(4,3);
+    lcd.print("Vpont=");
+    lcd.print(wheatstone,0);
+    lcd.print("mV  ");
+    delay(50);
+
          
-   lcd.clear();
-   lcd.setCursor(0,1);
-   lcd.print(" Capteur He OK");           
-   delay(2000);
-   lcd.clear();
-   
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print(" Capteur He OK");
+  delay(2000);
+  lcd.clear();
+
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   
   int16_t adc0;
@@ -139,90 +141,100 @@ void loop() {
   int EAD = 0;                  // prof equivalente Air pour narcose 30m
   float helium = 0;
   
-    nitrox = voltage * (20.9 / TensionCalib);
-    MOD = 10 * ( (160/nitrox) - 1);
+  nitrox = voltage * (20.9 / TensionCalib);
+  MOD = 10 * ( (160/nitrox) - 1);
     
-    lcd.setCursor(0,0);
-    if (voltage > 1) {
-      lcd.print("Oxygene = ");
-      lcd.print(nitrox,1);
-      lcd.print("%   ");
-      lcd.setCursor(12,2);
-      lcd.print(" MOD");
-      lcd.print(MOD);
-      lcd.print("m");
-      if (MOD < 100) {
-         lcd.print(" "); 
-         }
-      }
-      else {
-        lcd.print("Cell O2 HS !!! ");
-        }
-    
-    lcd.setCursor(0,3);    
-    lcd.print("Vpont=");
-    lcd.print(wheatstone,0);
-    lcd.print("mV  "); 
-
-    wheatstone = wheatstone - WheatCalib;  
-    
-    if (time < 480000) {  CorrFroid = 1 ; }     // correction de la tension lue en fonction du temps de chauffe du capteur 
-    if (time < 360000) {  CorrFroid = 2 ; }
-    if (time < 300000) {  CorrFroid = 3 ; }
-    if (time < 270000) {  CorrFroid = 4 ; }
-    if (time < 240000) {  CorrFroid = 5 ; }
-    if (time < 210000) {  CorrFroid = 6 ; }
-    if (time < 180000) {  CorrFroid = 7 ; }
-    if (time < 165000) {  CorrFroid = 8 ; }
-    if (time < 150000) {  CorrFroid = 9 ; }
-    if (time < 120000) {  CorrFroid = 10 ; }
-    if (time < 105000) {  CorrFroid = 11 ; }
-    if (time < 90000) {  CorrFroid = 12 ; }
-    if (time < 80000) {  CorrFroid = 13 ; }
-    if (time < 70000) {  CorrFroid = 14 ; }
-    if (time < 60000) {  CorrFroid = 15 ; }
-    if (time < 50000) {  CorrFroid = 16 ; }
-    if (time < 40000) {  CorrFroid = 17 ; }
-    if (time < 30000) {  CorrFroid = 18 ; }
-    
-    wheatstone = wheatstone - CorrFroid;            // ajustement car capteur pas assez chaud
-   
-    lcd.setCursor(0,1);    
-    lcd.print("Helium = ");
-    helium = 100 * wheatstone / calibMD62;
-    if (helium > 50) {
-      helium = helium * (1 + (helium - 50) * 0.4 / 100);
-      }
-    if (helium > 2) {
-      lcd.print(helium,1);
-      lcd.print("%    "); 
-      }
-    else {
-      helium = 0;
-      lcd.print("0%    ");  
-      }
-     lcd.setCursor(0,2);  
-    if (helium > 0) {  
-      lcd.print("Trimix ");
-      lcd.print(nitrox,0);
-      lcd.print("/");
-      lcd.print(helium,0);
+  lcd.setCursor(0,0);
+  if (voltage > 1) {
+    lcd.print("Oxygene = ");
+    lcd.print(nitrox,1);
+    lcd.print("%   ");
+    lcd.setCursor(12,2);
+    lcd.print(" MOD");
+    lcd.print(MOD);
+    lcd.print("m");
+    if (MOD < 100)
+    {
       lcd.print(" ");
-      lcd.setCursor(12,3);
-      lcd.print(" EAD");
-      EAD = 10 * ( 100 * 3.2 / (100 - helium - nitrox) -1 );
-      lcd.print(EAD);
-      lcd.print("m");  
-      if (EAD < 100) {
-         lcd.print(" "); 
-         } 
-      }
-     else {
-      lcd.print("Nitrox ");
-      lcd.print(nitrox,0);
-      lcd.print("    ");
-      lcd.setCursor(12,3);
-      lcd.print("       ");
-      }  
+     }
+  }
+  else
+  {
+    lcd.print("Cell O2 HS !!! ");
+  }
+
+  lcd.setCursor(0,3);
+  lcd.print("Vpont=");
+  lcd.print(wheatstone,0);
+  lcd.print("mV  ");
+
+  wheatstone = wheatstone - WheatCalib;
+    
+  if (time < 480000) {  CorrFroid = 1 ; }     // correction de la tension lue en fonction du temps de chauffe du capteur
+  if (time < 360000) {  CorrFroid = 2 ; }
+  if (time < 300000) {  CorrFroid = 3 ; }
+  if (time < 270000) {  CorrFroid = 4 ; }
+  if (time < 240000) {  CorrFroid = 5 ; }
+  if (time < 210000) {  CorrFroid = 6 ; }
+  if (time < 180000) {  CorrFroid = 7 ; }
+  if (time < 165000) {  CorrFroid = 8 ; }
+  if (time < 150000) {  CorrFroid = 9 ; }
+  if (time < 120000) {  CorrFroid = 10 ; }
+  if (time < 105000) {  CorrFroid = 11 ; }
+  if (time < 90000) {  CorrFroid = 12 ; }
+  if (time < 80000) {  CorrFroid = 13 ; }
+  if (time < 70000) {  CorrFroid = 14 ; }
+  if (time < 60000) {  CorrFroid = 15 ; }
+  if (time < 50000) {  CorrFroid = 16 ; }
+  if (time < 40000) {  CorrFroid = 17 ; }
+  if (time < 30000) {  CorrFroid = 18 ; }
+
+  wheatstone = wheatstone - CorrFroid;            // ajustement car capteur pas assez chaud
+   
+  lcd.setCursor(0,1);
+  lcd.print("Helium = ");
+  helium = 100 * wheatstone / calibMD62;
+  if (helium > 50)
+  {
+    helium = helium * (1 + (helium - 50) * 0.4 / 100);
+  }
+  if (helium > 2)
+  {
+    lcd.print(helium,1);
+    lcd.print("%    ");
+  }
+  else
+  {
+    helium = 0;
+    lcd.print("0%    ");
+  }
+
+  lcd.setCursor(0,2);
+  if (helium > 0)
+  {
+    lcd.print("Trimix ");
+    lcd.print(nitrox,0);
+    lcd.print("/");
+    lcd.print(helium,0);
+    lcd.print(" ");
+    lcd.setCursor(12,3);
+    lcd.print(" EAD");
+    EAD = 10 * ( 100 * 3.2 / (100 - helium - nitrox) -1 );
+    lcd.print(EAD);
+    lcd.print("m");
+    if (EAD < 100)
+    {
+      lcd.print(" ");
+    }
+  }
+  else
+  {
+    lcd.print("Nitrox ");
+    lcd.print(nitrox,0);
+    lcd.print("    ");
+    lcd.setCursor(12,3);
+    lcd.print("       ");
+  }
+
 }
 
